6a5b5e93605853e987d59ea06f340880bc1d2916
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2022-05-26 23:42:13 +0530

    ashitfxd

--- movlens/inpform/views.py
+++ movlens/inpform/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from .forms import InputForm
 # Create your views here.
 def say_hello(request):
     return render(request,'home.html')
 
 def genres(request):
+    # context ={}
+    # form= InputForm(request.POST)
+    # if form.is_valid():
+    
+    #     context = {'form': form}
+    
     return render(request,'main.html')
 
 def recomm(request):
-    return render(request,'recomm.html')
+    print(list(request.POST.items()))
+    context={}
+    for key,value in request.POST.items():
+        context[key]=value
+
+    return render(request,'recomm.html',context)
