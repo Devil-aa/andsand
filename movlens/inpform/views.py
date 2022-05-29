28b38ce691a48608ea42072ff46bccb844a7a9df
Devil-aa <abhi9559137@gmail.com>
2022-05-30 01:58:03 +0530

    done complete

--- movlens/inpform/views.py
+++ movlens/inpform/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 import random
+import imdb
+ia = imdb.IMDb()
 
 # Create your views here.
 def say_hello(request):
     return render(request,'home.html')
 
 def genres(request):
-    return render(request,'main.html')
+    return render(request,'checkbox.html')
 
 def keyw(request):
     return render(request,'test.html')
@@ -23,7 +25,35 @@ def recomm(request):
         df2=df2.where(df2['genres'].str.contains(i))
         df2=df2.dropna()
     l=list(df2['original_title'])
-    random.shuffle(l)
-    l=l[:11]
 
+    df2=df2.sample(frac = 1)
+    df3=df2.head(10)
+
+    l=[]
+    for i in range(len(list(df3['title']))):
+        t=[]
+        try:
+            search_results = ia.search_movie(list(df3['title'])[i])
+            if search_results:
+                movieID = search_results[0].movieID
+                movie = ia.get_movie(movieID)
+                
+                if movie:
+                    cast = movie.get('cast')
+                    topActors = 5
+                    for actor in cast[:topActors]:
+                        t.append("'{0} as {1}' ".format(actor['name'], actor.currentRole))
+        except:
+            pass
+
+
+        l.append({'title':list(df3['title'])[i],
+                'rating':str(list(df3['vote_average'])[i]),
+                'director':list(df3['director'])[i],
+                'runtime':str(list(df3['runtime'])[i]),
+                'date':list(df3['release_date'])[i],
+                'description':list(df3['overview'])[i]
+                ,'cast':t})
+                
+        #l.append([list(df3['title'])[i],list(df3['vote_average'])[i],list(df3['director'])[i],list(df3['runtime'])[i],list(df3['release_date'])[i],list(df3['overview'])[i],list(df3['cast'])[i]])
     return render(request,'recomm.html',{'l':l})
