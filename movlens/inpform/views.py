7093c55599b7bf41a6520bdb77148a9acb60ef16
Devil-aa <abhi9559137@gmail.com>
2022-05-29 16:32:00 +0530

    prog

--- movlens/inpform/views.py
+++ movlens/inpform/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+import random
 
 # Create your views here.
 def say_hello(request):
@@ -12,4 +14,16 @@ def keyw(request):
     return render(request,'test.html')
 
 def recomm(request):
-    return render(request,'recomm.html')
+    gen=[i for i in request.POST]
+    gen=gen[1:]
+    
+    df2 = pd.read_csv("C:\\Users\\Agrawal\\Desktop\\movies.csv")
+
+    for i in gen:
+        df2=df2.where(df2['genres'].str.contains(i))
+        df2=df2.dropna()
+    l=list(df2['original_title'])
+    random.shuffle(l)
+    l=l[:11]
+
+    return render(request,'recomm.html',{'l':l})
